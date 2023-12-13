642907dfccd7a7b28cab91bfb4573ff7e4183e8f
gizembrasser <gizembrasser101@gmail.com>
2023-12-13 14:31:52 +0100

    add: rest of the CRUD endpoints for /bookings

--- src/routes/bookings.js
+++ src/routes/bookings.js
@@ -1,9 +1,24 @@
 import express from "express";
+import getBookings from "../services/bookings/getBookings.js";
+import getBookingById from "../services/bookings/getBookingById.js";
 import createBooking from "../services/bookings/createBooking.js";
+import updateBookingById from "../services/bookings/updateBookingById.js";
+import deleteBooking from "../services/bookings/deleteBooking.js";
 import notFoundErrorHandler from "../middleware/notFoundErrorHandler.js";
 import authMiddleware from "../middleware/auth.js";
 
 const router = express.Router();
+
+router.get("/", async (req, res, next) => {
+    try {
+        const { userId } = req.query;
+        const bookings = await getBookings(userId);
+
+        res.json(bookings);
+    } catch (error) {
+        next(error);
+    }
+});
 
 router.post("/", async (req, res, next) => {
     try {
@@ -15,5 +30,41 @@ router.post("/", async (req, res, next) => {
         next(error);
     }
 });
+
+router.get("/:id", async (req, res, next) => {
+    try {
+        const { id } = req.params;
+        const booking = await getBookingById(id);
+
+        res.status(200).json(booking);
+    } catch (error) {
+        next(error);
+    }
+}, notFoundErrorHandler);
+
+router.put("/:id", async (req, res, next) => {
+    try {
+        const { id } = req.params;
+        const { userId, propertyId, checkinDate, checkoutDate, numberOfGuests, totalPrice, bookingStatus } = req.body;
+        const updatedBooking = await updateBookingById(id, { userId, propertyId, checkinDate, checkoutDate, numberOfGuests, totalPrice, bookingStatus });
+
+        res.status(200).json(updatedBooking);
+    } catch (error) {
+        next(error);
+    }
+}, notFoundErrorHandler);
+
+router.delete("/:id", async (req, res, next) => {
+    try {
+        const { id } = req.params;
+        const deletedBookingId = await deleteBooking(id);
+
+        res.status(200).json({
+            message: `Booking with id ${deletedBookingId} was deleted.`
+        });
+    } catch (error) {
+        next(error);
+    }
+}, notFoundErrorHandler);
 
 export default router;
