5b3582b8b7bd4585f1ca01af292263a1960fe9db
gizembrasser <gizembrasser101@gmail.com>
2023-12-13 11:19:32 +0100

    add: rest of the CRUD endpoints for amenities

--- src/routes/amenities.js
+++ src/routes/amenities.js
@@ -17,4 +17,51 @@ router.get("/", async (req, res, next) => {
     }
 });
 
+router.post("/", async (req, res, next) => {
+    try {
+        const { name } = req.body;
+        const newAmenity = await createAmenity(name);
+
+        res.status(201).json(newAmenity);
+    } catch (error) {
+        next(error);
+    }
+});
+
+router.get("/:id", async (req, res, next) => {
+    try {
+        const { id } = req.params;
+        const amenity = await getAmenityById(id);
+
+        res.status(200).json(amenity);
+    } catch (error) {
+        next(error);
+    }
+}, notFoundErrorHandler);
+
+router.put("/:id", async (req, res, next) => {
+    try {
+        const { id } = req.params;
+        const { name } = req.body;
+        const updatedAmenity = await updateAmenityById(id, { name });
+
+        res.status(200).json(updatedAmenity);
+    } catch (error) {
+        next(error);
+    }
+}, notFoundErrorHandler);
+
+router.delete("/:id", async (req, res, next) => {
+    try {
+        const { id } = req.params;
+        const deletedAmenityId = await deleteAmenity(id);
+
+        res.status(200).json({
+            message: `Amenity with id ${deletedAmenityId} was deleted.`
+        });
+    } catch (error) {
+        next(error);
+    }
+}, notFoundErrorHandler);
+
 export default router;
