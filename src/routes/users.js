e9423c7af5935d738f40f9e4d23f961f0ff285bd
gizembrasser <gizembrasser101@gmail.com>
2023-12-14 11:15:01 +0100

    add: PUT & DELETE routes

--- src/routes/users.js
+++ src/routes/users.js
@@ -1,6 +1,6 @@
 import express from "express";
-import getUsers from "../services/users/getUsers.js";
-import getUserById from "../services/users/getUserById.js";
+// import getUsers from "../services/users/getUsers.js";
+// import getUserById from "../services/users/getUserById.js";
 import createUser from "../services/users/createUser.js";
 import updateUserById from "../services/users/updateUserById.js";
 import deleteUser from "../services/users/deleteUser.js";
@@ -22,5 +22,32 @@ router.post("/", authMiddleware, async (req, res, next) => {
         next(error);
     }
 });
+
+// PUT route for updating a user by ID.
+router.put("/:id", authMiddleware, async (req, res, next) => {
+    try {
+        const { id } = req.params;
+        const { username, password, name, email, phoneNumber, profilePicture } = req.body;
+        const updatedUser = await updateUserById(id, { username, password, name, email, phoneNumber, profilePicture });
+
+        res.status(200).json(updatedUser);
+    } catch (error) {
+        next(error);
+    }
+}, notFoundErrorHandler);
+
+// DELETE route for deleting a user by ID.
+router.delete("/:id", authMiddleware, async (req, res, next) => {
+    try {
+        const { id } = req.params;
+        const deletedUserId = await deleteUser(id);
+
+        res.status(200).json({
+            message: `User with id ${deletedUserId} was deleted.`
+        });
+    } catch (error) {
+        next(error);
+    }
+}, notFoundErrorHandler);
 
 export default router;
--- src/services/users/deleteUser.js
+++ src/services/users/deleteUser.js
@@ -3,11 +3,12 @@ import NotFoundError from "../../errors/NotFoundError.js";
 
 const deleteUser = async (id) => {
     const prisma = new PrismaClient();
-    const user = await prisma.user.deleteMany({
+
+    const deleteUser = await prisma.user.deleteMany({
         where: { id }
     })
 
-    if (!user || user.count === 0) {
+    if (!deleteUser || deleteUser.count === 0) {
         throw new NotFoundError("User", id);
     }
 
